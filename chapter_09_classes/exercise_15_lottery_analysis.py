dcc5e5662da4973716164fcce09c1adbb2b0879a
Andreas Landerer <andreaslanderer@users.noreply.github.com>
2023-11-06 18:07:13 +0100

    Chapter 10: Classes (Used 'in' instead of try/except)

--- chapter_09_classes/exercise_15_lottery_analysis.py
+++ chapter_09_classes/exercise_15_lottery_analysis.py
@@ -6,16 +6,17 @@ lotteries = 0
 won = False
 while not won:
     lotteries += 1
+    score = 0
+    played_ticket = my_ticket[:]
     print(f"Playing lottery no.: {lotteries}")
-    my_ticket_copy = my_ticket[:]
     for i in range(4):
         chosen_number = choice(lottery_numbers)
-        try:
-            my_ticket_copy.remove(chosen_number)
-        except ValueError:
-            break
-    if len(my_ticket_copy) == 0:
+        print(chosen_number)
+        if chosen_number in played_ticket:
+            score += 1
+            played_ticket.remove(chosen_number)
+    if score == 4:
         won = True
         print("-> Congratulations! You won the lottery!")
     else:
-        print(f"-> You missed {len(my_ticket_copy)} numbers!")
+        print(f"-> You missed {4 - score} number(s)!")
